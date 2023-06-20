1804f2b6ecd01a34a44c66b1abafb8d99c2bf829
William <anazawilliam1@gmail.com>
2023-06-20 11:03:18 +0100

    Login component

--- src/views/Login.jsx
+++ src/views/Login.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React from "react";
+import { Footer, Form, Nav } from "../components/Index";
+import {
+  AiFillApple,
+  AiFillGoogleCircle,
+  AiOutlineGoogle,
+} from "react-icons/ai";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <main id="login__section">
+      <Nav />
+      <section className="container">
+        <article className="top__section">
+          <small>Log in with:</small>
+          <aside className="cta">
+            <button className="btn">
+              <AiOutlineGoogle className="icon" /> Google
+            </button>
+            <button className="btn">
+              <AiFillApple className="icon" /> Apple
+            </button>
+          </aside>
+        </article>
+        <p>or</p>
+        <article className="form">
+          <form id="form">
+            <input
+              type="email"
+              name=""
+              id=""
+              placeholder="Email address or username"
+            />
+            <input type="password" placeholder="Password" />
+          </form>
+        </article>
+      </section>
+      <Footer />
+    </main>
+  );
+};
 
-export default Login
+export default Login;
